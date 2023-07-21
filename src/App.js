950e430f2fa1a2e363d856247022b9a4905ed476
bubun123-git <bubundcool@gmail.com>
2023-07-21 14:25:20 +0530

    added rendered product div

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 import SellingComponent from './Components/SellingComponent';
 
+let eventhandler = (event) => {
+  event.preventDefault()
+}
+
 function App() {
   return (
-    <div classname="App-link">
-      <h1>Seller Admin Page</h1>
-      <SellingComponent></SellingComponent>
+    <form onSubmit={eventhandler}>
+      <div classname="App-link">
+        <h1>Seller Admin Page</h1>
+        <SellingComponent></SellingComponent>
 
-    </div>
+      </div>
+    </form>
+
   );
 }
 
